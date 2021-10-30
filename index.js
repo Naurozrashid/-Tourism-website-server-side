3f2eef5cfa0342595f0ca2f5f1493d76eee6d0c0
Nauroz <naurozrashid31@gmail.com>
2021-10-31 00:20:13 +0600

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const app = express();
-const port = process.env.Port || 5000;
+const port = process.env.PORT || 5000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sztg1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
