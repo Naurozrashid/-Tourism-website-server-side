fde551e3e76117f5a30e900c5e8c5c1b91934ac7
Nauroz <naurozrashid31@gmail.com>
2021-10-31 04:58:06 +0600

    get sinle data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 const cors=require('cors')
 require('dotenv').config();
 const app = express();
@@ -28,6 +29,16 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services);
         });
+
+        //Get Single Service
+
+             app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('getting specific service', id);
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        })
 
        
 
